aa3a735c321432007cd3d32373c5fb81f234e797
sgnawali25 <sandeshgyawali25@gmail.com>
2024-09-15 12:38:39 -0500

    education update

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -4,9 +4,9 @@ const Education = () => (
   <section id="education">
     <h2>Education</h2>
     <ul>
-      <li>Fisk University - Computer Science (Expected Graduation: 2025)</li>
-      <li>Relevant Courses: Data Structures & Algorithms, Machine Learning, Computer Organization, Data Science, Quantum Computing, Introduction to 
-        Computer Science I (Python) & II (Java), Calculus I and II, Discrete Mathematics, Linear Algebra
+      <li>Fisk University - Computer Science (Expected Graduation: 2026)</li>
+      <li>Relevant Courses: Data Structures & Algorithms, Machine Learning, Computer Organization, Operating System, Data Science, Quantum Computing, Introduction to 
+        Computer Science I (Python) & II (Java), Calculus I and II, Discrete Mathematics, Linear Algebra, Intermediate Web Development (CodePath), CyberSecurity (CodePath)
       </li>
     </ul>
   </section>
