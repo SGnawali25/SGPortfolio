6be91ccf808ef6a33b44223918d3ec19c55cd097
sgnawali25 <sandeshgyawali25@gmail.com>
2025-10-06 08:45:51 -0500

    implemented analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import ReactGA from 'react-ga4'
 import App from './App.jsx'
 import './index.css'
+
+ReactGA.initialize('G-0N9DGW93YN') 
+ReactGA.send('pageview')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
